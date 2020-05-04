8989cf5e5a0a5e2179aa7058ff317318f673fde6
liyifei <466885667@qq.com>
2020-05-04 16:51:24 +0800

    first add

---
+++ src/main/java/cn/itcast/travel/web/servlet/RegistUserServlet.java
@@ -0,0 +1,21 @@
+package cn.itcast.travel.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/RegistUserServlet")
+public class RegistUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        this.doPost(request, response);
+    }
+}
